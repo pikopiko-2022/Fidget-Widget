cb91ee49e107bba2a24949af34995c04113da797
Ben <8870073+ben-dodd@users.noreply.github.com>
2022-08-26 09:35:37 +1200

    Change index

--- client/index.js
+++ client/index.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-import Stage from './components/bouncy-ball/Stage'
+import App from './components/App'
 
 document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(<Stage />, document.getElementById('app'))
+  ReactDOM.render(<App />, document.getElementById('app'))
 })
