19389e6e442d48c32c0da1b20ed3e60f1e5392dc
Angela Kitt <107589139+angela-kitt@users.noreply.github.com>
2022-08-26 12:51:03 +1200

    CSS positioning done

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,17 +1,16 @@
 import React from 'react'
+import Home from './Home'
 import Dragbox from './drag-box/Dragbox'
-import Header from './Header'
-import Widget from './Widget'
 import Stage from './bouncy-ball/Stage'
-import { Link, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Background from './background/Background'
-import Mood from './background/Mood'
 
 const App = () => {
   return (
     <>
       <Background>
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/bouncy-ball/" element={<Stage />} />
           <Route path="/drag-box" element={<Dragbox />} />
           {/* <Route path="/widget/:fidget" element={<Fidget />} /> */}
